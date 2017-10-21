b0354c74c3a85c351574e303282c34de9697c0e0
oalberto96 <oalberto96c@gmail.com>
2017-10-20 23:21:15 -0700

    Add http request script

--- Examples/SendHttpRequest.ino
+++ Examples/SendHttpRequest.ino
@@ -1,3 +1,15 @@
+/* Http message
+AT+CIPSTART="TCP","10.0.0.2",8000
+AT+CIPSEND=120
+POST /arduinodata HTTP/1.1
+Host: 10.0.0.2
+User-Agent: Arduino/1.0"
+Connection: close"
+Content-Length: 1600"
+number=5
+AT+CIPCLOSE
+*/
+
 #include <SoftwareSerial.h>
 SoftwareSerial esp8266(2,3);
 
@@ -7,30 +19,57 @@ const char* HOST = "10.0.0.2";
 const char* PORT = "8000";
 const char* TYPE_CONNECTION = "TCP";
 
-String data = "number=5";
+String http_body = "number=5";
 
 String http_header[] = {
-    "POST /arduinodata HTTP/1.1",
-    "Host: 10.0.0.2",
-    "User-Agent: Arduino/1.0",
-    "Connection: close",
-    "Content-Length: 1600",
+    "POST /arduinodata HTTP/1.1\n",
+    "Host: 10.0.0.2\n",
+    "User-Agent: Arduino/1.0\n",
+    "Connection: close\n",
+    "Content-Length: 1600\n",
     ""
 };
+
+bool findText(String text,String text_to_find) {
+    bool exist = false;
+    int index = text.indexOf(text_to_find);
+    if(index>=0){
+        exist = true;
+    }
+    return exist;
+}
+
+bool statusConnection(){
+    //3: Connected
+    //4: Disconnected
+    String response;
+    String status = "AT";
+    esp8266.println("AT+CIPSTATUS");
+    response = esp8266Listener();
+    return findText(response,status);
+
+}
 
 void establishConnection(String type, String ip_address, String port) {
     String command = String("AT+CIPSTART=") + "\""+ type+"\"" +"," + "\"" +ip_address + "\"" + "," + port;
     esp8266.println(command);
 }
 
-void sendHttpPost(String http_header[], String body) {
-
+void sendHttpRequest(String http_header[],int size_http_header, String body) {
+    establishConnection(TYPE_CONNECTION, HOST, PORT);
+    delay(20);
+    for (int i = 0; i < size_http_header; i++) {
+        sendPackage(http_header[i]);
+    }
+    sendPackage(http_body);
+    closeConnection();
 }
 
 void sendPackage(String package){
   esp8266.println("AT+CIPSEND=" + String(package.length()));
   delay(20);
   esp8266.println(package);
+  esp8266.println("\n");
 }
 
 void closeConnection(){
@@ -46,7 +85,7 @@ String esp8266Listener(){
         {
             c = esp8266.read();
             response.concat(c);
-            Serial.write(c);
+            //Serial.write(c);
         }
     }
     return response;
@@ -60,32 +99,16 @@ void setup()
 
 void loop()
 {
-    establishConnection(TYPE_CONNECTION, HOST, PORT);
+
     Serial.println("Waiting...");
-    esp8266Listener();
+    sendHttpRequest(http_header,5,http_body);
+    /*if(statusConnection()){
+        Serial.println("Conectado");
+    }
+    else {
+        Serial.println("No Conectado");
+        establishConnection(TYPE_CONNECTION, HOST, PORT);
+    }*/
     //Serial.write(esp8266Listener());
     delay(3000);
-    //esp8266.println("AT+CIPSTATUS");
-    //Serial.write(esp8266Listener());
-    delay(3000);
-    //esp8266Listener();
-
 }
-
-
-
-/* TCP
-AT+CIPSTART="TCP","10.0.0.2",8000
-AT+CIPSEND=5
-AT+CIPCLOSE
-
-*/
-/* HTTP
-client.println("POST /Api/AddParking/3 HTTP/1.1");
-client.println("Host: 10.0.0.138");
-client.println("User-Agent: Arduino/1.0");
-client.println("Connection: close");
-client.print("Content-Length: ");
-client.println(PostData.length());
-client.println();
-client.println(PostData);*/
